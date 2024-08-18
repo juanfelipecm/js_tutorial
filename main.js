e1ed4d43f7a213ff2fc66f57a928ede04d5f663b
Juan Felipe Castillo <juanfelipe.22@gmail.com>
2024-08-18 14:06:13 -0500

    Palindrome con empty string

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 let Phrase = require("jfcm-palindrome");
-let String = prompt("Ingresa una frase para evaluar si es un palindrome:");
 
-let phrase = new Phrase(String);
-if (phrase.palindrome()) {
-    alert (`"${phrase.content}" is a palindrome.`);
-} else {
-    alert (`"${phrase.content}" is not a palindrome.`);
+function palindromeTester(event) {
+    event.preventDefault();
+
+    let phrase = new Phrase(event.target.phrase.value);
+    let palindromeResult = document.querySelector("#palindromeResult");
+
+    if (phrase.palindrome()) {
+        palindromeResult.innerHTML = `"${phrase.content}" <strong>is</strong> a palindrome.`;
+    } else {
+        palindromeResult.innerHTML = `"${phrase.content}" is not a palindrome.`;
+    }
 }
+
+document.addEventListener("DOMContentLoaded", function(){
+    let tester = document.querySelector("#palindromeTester");
+    tester.addEventListener("submit", function() {
+        palindromeTester(event);
+    });
+});
+
+
 
